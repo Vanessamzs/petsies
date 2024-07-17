c1b2afbd0deab55db75c99bfc92e714d9a97f954
Joanny Prignol <joannyprignol@gmail.com>
2024-07-17 12:01:38 +0200

    correction de la card

--- app/javascript/controllers/pet_action_display_controller.js
+++ app/javascript/controllers/pet_action_display_controller.js
@@ -1,30 +1,64 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="pet-action-display"
 let informationsActive = false;
 let modificationActive = false;
+let addEventActive = false;
 
 export default class extends Controller {
-
-  static targets = ["petInformationsContainer", "petModificationContainer"];
+  static targets = ["petInformationsContainer", "petModificationContainer", "petAddEventContainer"];
 
   toggleInformations() {
-    this.petInformationsContainerTarget.classList.toggle('card-pet-informations-display');
-    informationsActive = !informationsActive;
-
-    if (modificationActive) {
-      this.petModificationContainerTarget.classList.toggle('card-pet-modification-display');
-      modificationActive = false;
+    if (!informationsActive) {
+      if (modificationActive) {
+        this.petModificationContainerTarget.classList.remove('card-pet-modification-display');
+        modificationActive = false;
+      }
+      if (addEventActive) {
+        this.petAddEventContainerTarget.classList.remove('card-pet-add-event-display');
+        addEventActive = false;
+      }
+      this.petInformationsContainerTarget.classList.add('card-pet-informations-display');
+      informationsActive = true;
+    } else {
+      this.petInformationsContainerTarget.classList.remove('card-pet-informations-display');
+      informationsActive = false;
     }
   }
 
   toggleModification() {
-    this.petModificationContainerTarget.classList.toggle('card-pet-modification-display');
-    modificationActive = !modificationActive;
+    if (!modificationActive) {
+      if (informationsActive) {
+        this.petInformationsContainerTarget.classList.remove('card-pet-informations-display');
+        informationsActive = false;
+      }
+      if (addEventActive) {
+        this.petAddEventContainerTarget.classList.remove('card-pet-add-event-display');
+        addEventActive = false;
+      }
+      this.petModificationContainerTarget.classList.add('card-pet-modification-display');
+      modificationActive = true;
+    } else {
+      this.petModificationContainerTarget.classList.remove('card-pet-modification-display');
+      modificationActive = false;
+    }
+  }
 
-    if(informationsActive) {
-      this.petInformationsContainerTarget.classList.toggle('card-pet-informations-display');
-      informationsActive = false;
+  toggleAddEvent() {
+    if (!addEventActive) {
+      if (informationsActive) {
+        this.petInformationsContainerTarget.classList.remove('card-pet-informations-display');
+        informationsActive = false;
+      }
+      if (modificationActive) {
+        this.petModificationContainerTarget.classList.remove('card-pet-modification-display');
+        modificationActive = false;
+      }
+      this.petAddEventContainerTarget.classList.add('card-pet-add-event-display');
+      addEventActive = true;
+    } else {
+      this.petAddEventContainerTarget.classList.remove('card-pet-add-event-display');
+      addEventActive = false;
     }
   }
 }
