e0d171a2d4b88b6e99581d85025220a3d441961c
vanessa mazeas <vanessa.ferrere@live.fr>
2024-07-17 11:10:45 +0200

    controllers more

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,42 @@
+class EventsController < ApplicationController
+  before_action :set_pet
+
+  def index
+    @events = pet.events
+  end
+
+  def show
+  end
+
+  def new
+    @event = @pet.events
+    @event = Event.new
+  end
+
+  def create
+    @event = @pet.events.new(event_params)
+    if @event.save
+      redirect_to pet_path(@pet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = @pet.events.find(params[:id])
+    @event.destroy
+    redirect_to root_path, notice: 'evènement supprimé avec succès'
+  end
+
+  private
+
+  def set_pet
+    @pet = Pet.find(params[:pet_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Pet not found."
+  end
+
+  def event_params
+    params.require(:event).permit(:event_type, :treatment_type, :description, :date)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pets#index"
- 
-  resources :pets, except: [:index]
-  
+
+  resources :pets, except: [:index] do
+    resources :events
+  end
+  resources :user_pet
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
