004f17b7b3d921a59db6da3efa6c2796586913d5
Tom Ardouin <tom.ardouin@icloud.com>
2024-07-19 12:23:59 +0200

    calendar dans le footer, notif de tous les animaux et show event
    Signed-off-by: Tom Ardouin <tom.ardouin@icloud.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pets#index"
 
+  get "calendar", to: "pages#calendar"
+
   resources :pets, except: [:index] do
     resources :events
   end
