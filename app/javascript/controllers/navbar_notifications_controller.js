8c8cc0d36fd099b3062f0fa3f32d91a6665fc6a4
Joanny Prignol <joannyprignol@gmail.com>
2024-07-23 16:34:28 +0200

    création du volet de notifications et de la pastille de notifications

--- app/javascript/controllers/navbar_notifications_controller.js
+++ app/javascript/controllers/navbar_notifications_controller.js
@@ -2,20 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar-notifications"
 export default class extends Controller {
-  static targets = ["notificationsDisplay", "toggleButton"]
+  static targets = ["notificationsDisplay", "toggleButton", "toggleNumberButton"]
 
   toggle() {
     const bellIconHTML = '<i class="fa-solid fa-bell fa-xl fa-rotate-by" style="color: #fffbf4; --fa-rotate-angle: 25deg;"></i>';
     const closeIconHTML = '<i class="fa-solid fa-x fa-xl" style="color: #fffbf4;"></i>';
 
-    this.notificationsDisplayTarget.classList.toggle('transition');
+    this.toggleNumberButtonTarget.classList.toggle("hidden");
 
     if (this.toggleButtonTarget.innerHTML.trim() === bellIconHTML.trim()) {
       this.toggleButtonTarget.innerHTML = closeIconHTML;
-      this.notificationsDisplayTarget.classList.toggle('show-notifications');
     } else if (this.toggleButtonTarget.innerHTML.trim() === closeIconHTML.trim()) {
       this.toggleButtonTarget.innerHTML = bellIconHTML;
-      this.notificationsDisplayTarget.classList.toggle('show-notifications');
     }
   }
 }
